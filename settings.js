e861bd5fd64d39f7c8b350efe7017c3e7ab686ea
ASITHAMD-Yt <githubbot715@gmail.com>
2025-06-07 20:48:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,5 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : procDwx0GQwJ#DGQ4GuMuLaRKHfAvj4OpKOloEDVQfJOTnTrnQvArg4sess.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
-POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
+POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'qkZ3gazA#kWQPYrU8-feyv8k49CN3s6zpU1cIwZychXe4hSJfWts' : process.env.POSTGRESQL_URL,
 };
